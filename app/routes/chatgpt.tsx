3d4ebaf956bc63154ca528e0a9afd1627add2b6a
Stefanos Orovas <stef.orovas@gmail.com>
2023-03-10 21:54:42 +0200

    feat(chatgpt): get suggestion from list of top songs

--- app/routes/chatgpt.tsx
+++ app/routes/chatgpt.tsx
@@ -1,23 +1,53 @@
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import type { ActionArgs } from "@remix-run/node";
 import { openai } from "~/services/chatgpt.server";
+import { spotifyApi } from "~/services/spotify.server";
+import { getSession } from "~/services/session.server";
+
+const basePrompt = `You are to act as the perfect music suggester\n 
+  I will provide you some previously liked songs on this format [song title, album name, artist] and every song will be in a new line.\n
+  Then you will suggest me only one new song that you think i may like and it is not in the list \n
+  Do not explain anything except for the song and the reason you selected it.\n
+  You will provide a link to search for the song on spotify. This is the format. https://open.spotify.com/search/artist:{ARTIST}%20track:{TRACK}\n
+  The previously liked songs are the following: \n`;
 
 export async function action({ request }: ActionArgs) {
+  let session = await getSession(request.headers.get("cookie"));
+  const accessToken = session.data["spotify:session"].accessToken;
+  spotifyApi.setAccessToken(accessToken);
+
+  const topTracks = await spotifyApi.getMyTopTracks();
+  const songs = topTracks.body.items;
+  const topSongs = songs
+    .map(
+      (s) =>
+        `song title: ${s.name}, album name: ${s.album.name}, artist: ${s.artists[0].name}`
+    )
+    .join("\n");
+  console.log(topSongs);
+
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Hello world",
+    prompt: [basePrompt, topSongs].join("\n"),
+    max_tokens: 100,
+    temperature: 0,
+    n: 1,
   });
-  console.log(completion.data.choices[0].text);
-  return null;
+
+  return { message: completion.data.choices[0].text };
 }
 
 export default function ChatGPT() {
+  const data = useActionData<typeof action>();
+
   return (
-    <Form action="." method="post">
-      <input type="text" name="message" />
-      <button type="submit" name="intent">
-        Send
-      </button>
-    </Form>
+    <>
+      <Form method="post">
+        <button type="submit" name="intent" value="get-me">
+          Get Suggestion
+        </button>
+      </Form>
+      {data?.message && <p>{data.message}</p>}
+    </>
   );
 }
