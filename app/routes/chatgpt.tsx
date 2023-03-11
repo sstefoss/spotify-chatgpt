a6e7ff648a0da9428c0cfa84958abcbdbd21fb3f
Stefanos Orovas <stef.orovas@gmail.com>
2023-03-11 19:32:00 +0200

    fix(chatgtp): prompt improvement

--- app/routes/chatgpt.tsx
+++ app/routes/chatgpt.tsx
@@ -6,9 +6,10 @@ import { getSession } from "~/services/session.server";
 
 const basePrompt = `You are to act as the perfect music suggester\n 
   I will provide you some previously liked songs on this format [song title, album name, artist] and every song will be in a new line.\n
-  Then you will suggest me only one new song that you think i may like and it is not in the list \n
+  Then you will suggest me only a new song that you think i may like and it is not in the list but if i ask you again you will give me a different one \n
   Do not explain anything except for the song and the reason you selected it.\n
   You will provide a link to search for the song on spotify. This is the format. https://open.spotify.com/search/artist:{ARTIST}%20track:{TRACK}\n
+  Be sure that what you suggest is a song and not an album or a playlist.\n
   The previously liked songs are the following: \n`;
 
 export async function action({ request }: ActionArgs) {
