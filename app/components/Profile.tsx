44bbadcf7547e5c6212815d8c14246fe4e8c5785
Stefanos Orovas <stef.orovas@gmail.com>
2023-03-14 11:53:57 +0200

    fix(logout): use logout action

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -7,7 +7,7 @@ export const Profile = () => {
   return (
     <div className="relative not-prose">
       <div
-        className="flex items-center rounded-full bg-black pr-2 pl-0.5 cursor-pointer hover:bg-gray-800"
+        className="flex items-center rounded-full bg-black pr-2 pl-0.5 py-0.5 cursor-pointer hover:bg-gray-800"
         onClick={() => setIsOpen(!isOpen)}
       >
         <div className="rounded-full bg-gray-700 p-1.5 mr-2">
@@ -21,7 +21,7 @@ export const Profile = () => {
         <div className="absolute top-10 right-0 bg-gray-700 w-[200px] p-1.5 rounded-lg">
           <ul>
             <li className="cursor-pointer hover:bg-gray-600 px-4 py-1 rounded-lg">
-              <Form action="/logout" method="post">
+              <Form action="/action/logout" method="post">
                 <button name="action" value="logout">
                   Logout
                 </button>
