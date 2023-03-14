cf2836b6299c54a94f67883661dfbadd5b8e6968
Stefanos Orovas <stef.orovas@gmail.com>
2023-03-14 11:53:29 +0200

    fix(prisma): avoid recreating instances

--- app/services/data.server.ts
+++ app/services/data.server.ts
@@ -1,10 +1,8 @@
-import { json } from "@remix-run/node";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { prisma } from "~/lib/prisma";
 
 export async function like(id: number) {
   // like song
-  await prisma.recommendation.update({
+  return await prisma.recommendation.update({
     where: {
       id,
     },
@@ -17,7 +15,7 @@ export async function like(id: number) {
 
 export async function dislike(id: number) {
   // dislike song
-  await prisma.recommendation.update({
+  return await prisma.recommendation.update({
     where: {
       id,
     },
@@ -30,10 +28,9 @@ export async function dislike(id: number) {
 
 export async function getAllRecommendations() {
   // get all recommendations
-  const recommendations = await prisma.recommendation.findMany({
+  return await prisma.recommendation.findMany({
     include: {
       song: true,
     },
   });
-  return json(recommendations);
 }
